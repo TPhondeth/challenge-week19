7a22647a7d91e2e44c8974fd543489238451acc1
Tony Phondeth <phondeth@gmail.com>
2021-10-23 20:08:40 -0400

    connect mongodb

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
-
 const app = express();
 
 app.use(logger("dev"));
@@ -18,9 +16,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
+    useFindAndModify: false,
     useNewUrlParser: true,
-    useFindAndModify: false
+    useUnifiedTopology: true
 });
 
 // routes
